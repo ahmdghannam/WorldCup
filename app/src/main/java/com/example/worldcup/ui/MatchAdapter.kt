032a75c9823bf392dbb0a630dcb496013625e200
Ahmed Ghannam <iia7mt@gmail.com>
2023-03-30 22:23:45 +0300

    0098 Multi view types inside Recycler View

--- app/src/main/java/com/example/worldcup/ui/MatchAdapter.kt
+++ app/src/main/java/com/example/worldcup/ui/MatchAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.worldcup.ui
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,17 +8,42 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.worldcup.R
 import com.example.worldcup.data.domain.Match
 import com.example.worldcup.databinding.ItemMatchBinding
+import com.example.worldcup.databinding.ItemMatchesHeaderBinding
 
 
 class MatchAdapter(private var list: List<Match>, private val listener: MatchInteractionListener) :
-    RecyclerView.Adapter<MatchAdapter.MatchViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_match, parent, false)
-        return MatchViewHolder(view)
+    RecyclerView.Adapter<MatchAdapter.BaseViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+       return when(viewType){
+            VIEW_TYPE_HEADER ->{
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_matches_header,parent,false)
+                 HeaderViewHolder(view)
+            }
+            VIEW_TYPE_MATCH -> {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_match, parent, false)
+             MatchViewHolder(view)
+            }
+            else -> super.createViewHolder(parent,viewType)
+        }
     }
 
-    override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         val currentMatch = list[position]
+        when(holder){
+            is MatchViewHolder ->{
+                bindMatch(holder, currentMatch)
+            }
+            else -> {}
+        }
+
+    }
+
+    private fun bindMatch(
+        holder: MatchViewHolder,
+        currentMatch: Match
+    ) {
         holder.binding.apply {
             tvHomeTeam.text = currentMatch.homeTeamName
             tvAwayTeam.text = currentMatch.awayTeamName
@@ -37,16 +61,31 @@ class MatchAdapter(private var list: List<Match>, private val listener: MatchInt
     }
 
 
-
     fun setData(newList: List<Match>) {
         val diffResult = DiffUtil.calculateDiff(MatchDifferentUtility(list, newList))
         list = newList
         diffResult.dispatchUpdatesTo(this)
     }
 
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position==0) VIEW_TYPE_HEADER
+        else VIEW_TYPE_MATCH
+    }
     override fun getItemCount() = list.size
 
-    class MatchViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+    sealed class BaseViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem)
+
+    class MatchViewHolder(viewItem: View) : BaseViewHolder(viewItem) {
         val binding = ItemMatchBinding.bind(viewItem)
+    }
+
+    class HeaderViewHolder(viewItem: View) : BaseViewHolder(viewItem) {
+        val binding = ItemMatchesHeaderBinding.bind(viewItem)
+    }
+
+    companion object{
+        const val VIEW_TYPE_HEADER=11
+        const val VIEW_TYPE_MATCH=12
     }
 }
